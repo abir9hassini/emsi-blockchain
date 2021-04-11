71a709f85ae09730d6adc9c096eda690c952d26c
hassini9abir <hassini.abir97@gmail.com>
2021-04-11 00:28:56 +0000

    +provided&test directories

--- holbertonschool-blockchain/crypto/test/sha256-main.c
+++ holbertonschool-blockchain/crypto/test/sha256-main.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "hblk_crypto.h"
 
 void _print_hex_buffer(uint8_t const *buf, size_t len);
