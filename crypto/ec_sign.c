40defe5190c9c0f33e86fd217d2995f5e465fa83
hassini9abir <hassini.abir97@gmail.com>
2021-04-18 15:21:59 +0000

    ec_sign.c modification

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,31 +1,27 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
 #include "hblk_crypto.h"
 
 /**
- * ec_sign - sign a set of bytes, using given private EC_KEY
- * @key: pointer to EC_KEY struct containing private key to perform the signing
- * @msg: pointer to characters to be signed
- * @msglen: len of msg
- * @sig: address to store signature
- *
- * Return: pointer to signature buffer on success, NULL on error
- */
+* ec_sign - sign msg with EC_KEY pair
+* @key: key pair
+* @msg: message to sign
+* @msglen: len of message
+* @sig: signature
+* Return: buffer to signature
+*/
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
-	size_t msglen, sig_t *sig)
+size_t msglen, sig_t *sig)
 {
+uint32_t len = 0;
+
 if (!key || !msg || !msglen)
 return (NULL);
-bzero(sig->sig, sizeof(sig->sig));
-sig->len = 0;
-if (!ECDSA_sign(0, msg, msglen, sig->sig,
-		(unsigned int *)&sig->len, (EC_KEY *)key))
+
+memset(sig->sig, 0, sizeof(sig->sig));
+if (!ECDSA_sign(0, msg, (int)msglen, sig->sig, &len, (EC_KEY *)key))
 {
 sig->len = 0;
 return (NULL);
 }
-return ((uint8_t *)sig->sig);
+sig->len = (uint8_t)len;
+return (sig->sig);
 }
-
