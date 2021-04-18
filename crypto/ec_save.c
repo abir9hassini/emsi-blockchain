4e23181cc68a320d445c23b731c1e04d4bd05701
hassini9abir <hassini.abir97@gmail.com>
2021-04-18 01:28:52 +0000

    re try 2

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,5 +1,12 @@
 #include "hblk_crypto.h"
 
+/**
+ * ec_save - save existing EC key pair to disk
+ * @key: EC key pair to save
+ * @folder: path to folder to save keys
+ *
+ * Return: 1 on success, 0 on error
+ */
 int ec_save(EC_KEY *key, char const *folder)
 {
 char buf[BUFSIZ];
@@ -13,20 +20,18 @@ fp = fopen(buf, "w");
 if (!fp)
 return (0);
 if (!PEM_write_EC_PUBKEY(fp, key))
-{
-fclose(fp);
-return (0);
-}
+goto out;
 fclose(fp);
 sprintf(buf, "%s/%s", folder, PRI_FILENAME);
 fp = fopen(buf, "w");
 if (!fp)
 return (0);
 if (!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL))
-{
+goto out;
+fclose(fp);
+return (1);
+out:
 fclose(fp);
 return (0);
 }
-fclose(fp);
-return (1);
-}
+
