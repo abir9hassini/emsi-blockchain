4d3b6ec9b6d58beabc8acff93ce75d33adf9732b
hassini9abir <hassini.abir97@gmail.com>
2021-05-11 22:28:37 +0000

    v0.2 project structure

---
+++ blockchain/v0.2/test/blockchain_create-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <llist.h>
+
+#include "blockchain.h"
+
+
+void _blockchain_print(Blockchain const *blockchain);
+
+int main(void)
+{
+Blockchain *blockchain;
+Blockchain *blockchain2;
+
+blockchain = blockchain_create();
+
+_blockchain_print(blockchain);
+
+return (EXIT_SUCCESS);
+}
+
