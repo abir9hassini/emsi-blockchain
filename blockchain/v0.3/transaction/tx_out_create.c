2809d017d1000e956ab131cdab4604d26358924c
hassini9abir <hassini.abir97@gmail.com>
2021-05-11 23:41:42 +0000

    tx_out_create.c modification

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -8,22 +8,13 @@
  */
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
-tx_out_t *new_tx_output = NULL;
+tx_out_t *t = calloc(1, sizeof(*t));
 
-new_tx_output = calloc(1, sizeof(tx_out_t));
-
-if (new_tx_output == NULL)
-{
+if (!t)
 return (NULL);
+t->amount = amount;
+memcpy(t->pub, pub, sizeof(t->pub));
+if (!sha256((int8_t const *)t, sizeof(t->amount) + sizeof(t->pub), t->hash))
+return (free(t), NULL);
+return (t);
 }
-new_tx_output->amount = amount;
-memcpy(new_tx_output->pub, pub, sizeof(new_tx_output->pub));
-if (!sha256((int8_t const *)new_tx_output,
-sizeof(new_tx_output->amount) + sizeof(new_tx_output->pub),
-new_tx_output->hash))
-{
-free(new_tx_output);
-return (NULL);
-}
-return (new_tx_output);
-
