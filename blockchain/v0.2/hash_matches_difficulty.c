850670045c0584c828397a4d375e0c549eb2cfd3
hassini9abir <hassini.abir97@gmail.com>
2021-05-15 05:20:13 +0000

    hash_matches_difficulty.c modification using Betty-style

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -7,17 +7,21 @@ int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 uint32_t difficulty)
 {
 uint32_t cpt = 0;
-int i=0;int bit=7;
-{for ( i = 0; i < SHA256_DIGEST_LENGTH; i++)
- for(bit=7;bit>=0;bit--){
- if(hash[i] & 1 << bit){
- break;
- }
- else{ cpt++;}
- }
- }
- if(cpt >= difficulty) return (1);
- return (0);
+int i = 0;
+int bit = 7;
+{
+for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
+for (bit = 7; bit >= 0; bit--)
+{
+if (hash[i] & 1 << bit)
+break;
+else
+cpt++;
+}
+}
+if (cpt >= difficulty)
+return (1);
+return (0);
 }
 
 
