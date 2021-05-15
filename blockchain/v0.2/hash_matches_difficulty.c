b10fb055cb41267a3fd62945a9bf57b9b9c7e5ce
hassini9abir <hassini.abir97@gmail.com>
2021-05-15 05:58:04 +0000

    types modification

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -7,8 +7,8 @@ int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 uint32_t difficulty)
 {
 uint32_t cpt = 0;
-int i = 0;
-int bit = 7;
+uint32_t i = 0;
+uint32_t bit = 7;
 {
 for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
 for (bit = 7; bit >= 0; bit--)
