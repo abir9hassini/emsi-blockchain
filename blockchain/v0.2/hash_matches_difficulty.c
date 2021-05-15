ad340dabc7c37ea49d162fc9c295754ab71dbc0e
hassini9abir <hassini.abir97@gmail.com>
2021-05-15 18:35:51 +0000

    hash_matches_difficulty.c file modification

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -9,11 +9,12 @@ uint32_t difficulty)
 uint32_t cpt = 0;
 int i = 0;
 int bit = 7;
-{
+
 for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
+{
 for (bit = 7; bit >= 0; bit--)
 {
-if (hash[i] & (1 << bit))
+if (hash[i] & 1 << bit)
 break;
 else
 cpt++;
