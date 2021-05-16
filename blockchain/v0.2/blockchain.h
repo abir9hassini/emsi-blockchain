d85bafc048d265780a56fdbe0a8df59d9b7d794f
hassini9abir <hassini.abir97@gmail.com>
2021-05-16 00:11:04 +0000

    modification block_mine

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -90,5 +90,6 @@ blockchain_t *blockchain_deserialize(char const *path);
 int block_is_valid(block_t const *block, block_t const *prev_block);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 uint32_t difficulty);
+void block_mine(block_t *block);
 #endif
 
--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -6,27 +6,32 @@
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 uint32_t difficulty)
 {
-uint32_t cpt = 0;
-int i = 0;
-int bit = 7;
+int i = 0, bit = 0, one_found = 0, copy_difficulty = 0;
 
+copy_difficulty = difficulty;
+/*Iterate every byte*/
 for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
 {
+/*Check every bit from left to right*/
 for (bit = 7; bit >= 0; bit--)
 {
+/*If we foudn something different to zero stop of iterate*/
 if (hash[i] & 1 << bit)
+{
+one_found = 1;
 break;
-else
-cpt++;
+}
+copy_difficulty--;
+}
+if (one_found == 1)
+{
+break;
 }
 }
-if (cpt >= difficulty)
+/*If the zeros from left to right is greater than difficulty return 1else0*/
+if (copy_difficulty <= 0)
+{
 return (1);
-else
+}
 return (0);
 }
-
-
-
-
-
