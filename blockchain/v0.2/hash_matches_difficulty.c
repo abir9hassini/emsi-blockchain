9b3a7eae5b5da63cea145a5c9d6cae4a16eaf826
hassini9abir <hassini.abir97@gmail.com>
2021-05-16 22:16:56 +0000

    another version of this code: fixed

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,37 +1,40 @@
-#include<stdio.h>
 #include "blockchain.h"
-
-
-
+/**
+* hash_matches_difficulty - checks if hash matches difficulty
+* @hash: hash to check
+* @difficulty: difficulty of a block
+* Return: 1 if hash matches difficulty, otherwise 0
+*/
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 uint32_t difficulty)
 {
-int i = 0, bit = 0, one_found = 0, copy_difficulty = 0;
+uint8_t i, bit, cpt = 0;
 
-copy_difficulty = difficulty;
 /*Iterate every byte*/
 for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
-{
+/*check if every byte == "00" */
+if (!hash[i])
+cpt += 8;
+else
+break;
 /*Check every bit from left to right*/
-for (bit = 7; bit >= 0; bit--)
+for (bit = 1; bit < 8; bit++)
+/**
+* If we found something equals to zero we increment
+* the cpt variable by 8- bit
+*/
+if (hash[i] >> bit == 0)
 {
-/*If we found something different to zero stop of iterate*/
-if (hash[i] & 1 << bit)
-{
-one_found = 1;
+cpt += (8 - bit);
 break;
 }
-copy_difficulty--;
-}
-if (one_found == 1)
-{
-break;
-}
-}
-/*If the zeros from left to right is greater than difficulty return 1 else 0*/
-if (copy_difficulty <= 0)
-{
+/**
+* at the end , if we found that cpt < difficulty that
+* mean that the hash doesn't match the difficulty
+* or if cpt >= difficulty so that what we want and
+* we can say that the hash match the difficulty
+*/
+if (cpt >= difficulty)
 return (1);
-}
 return (0);
 }
