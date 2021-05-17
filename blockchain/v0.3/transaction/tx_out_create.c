f7139e36cb93ea78914beb0481e4bde489425a0d
abir9hassini <40744191+abir9hassini@users.noreply.github.com>
2021-05-17 11:02:31 +0100

    Update tx_out_create.c

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -1,20 +1,25 @@
 #include "transaction.h"
-
 /**
- * tx_out_create - creates a new transaction output structure
- * @amount: the transaction amount
- * @pub: public key of receiver
- * Return: pointer to new structure or NULL
- */
+ * tx_out_create - creates an output transaction
+ * Return: the trasaction output, or NULL if it fails.
+ * @amount: the amount in the transaction.
+ * @pub: the length of the public key.
+*/
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
-tx_out_t *t = calloc(1, sizeof(*t));
-
-if (!t)
+tx_out_t *tx_out = malloc(sizeof(tx_out_t));
+if (!pub || !tx_out)
+{
+free (tx_out);
 return (NULL);
-t->amount = amount;
-memcpy(t->pub, pub, sizeof(t->pub));
-if (!sha256((int8_t const *)t, sizeof(t->amount) + sizeof(t->pub), t->hash))
-return (free(t), NULL);
-return (t);
+}
+tx_out->amount = amount;
+memcpy(&tx_out->pub, &pub, EC_PUB_LEN);
+if (!sha256((const int8_t *)tx_out,
+sizeof(tx_out->pub) + sizeof(tx_out->amount), tx_out->hash))
+{
+free (tx_out);
+return (NULL);
+}
+return (tx_out);
 }
