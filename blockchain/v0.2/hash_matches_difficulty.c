00639c179e51bf8e8bef181d4fce933b76d3644a
abir9hassini <40744191+abir9hassini@users.noreply.github.com>
2021-05-18 01:44:56 +0100

    Update hash_matches_difficulty.c
    adding comments

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -10,19 +10,27 @@ uint32_t difficulty)
 {
 uint8_t i, bit, cpt = 0;
 
+/* Iterate the hash table byte by byte */ 
 for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
+/* if a byte ==0 we increment directly the cpt */ 
 if (!hash[i])
 cpt += 8;
 else
+/* else , we check bit by bit if hash[i] >> bit is equal to 00000000 */
 break;
 for (bit = 1; bit < 8; bit++)
 if (hash[i] >> bit == 0)
 {
+/*in this case we increment cpt by 8 - bit */ 
 cpt += (8 - bit);
 break;
 }
-
-if (cpt < difficulty)
-return (0);
+/**
+* after checking all the bytes , if we found cpt >= difficulty 
+* we can say that the hash match difficulty , if not , the hash doesn't 
+* match the difficulty
+*/ 
+if (cpt >= difficulty)
 return (1);
+return (0);
 }
