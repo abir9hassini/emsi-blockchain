81314a9e66d4dbbf03319326dc90c0661156ef92
abir9hassini <40744191+abir9hassini@users.noreply.github.com>
2021-05-19 21:48:10 +0100

    Update blockchain.h

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -16,7 +16,7 @@
 #define BLOCK_GENERATION_INTERVAL 1
 #define DIFFICULTY_ADJUSTMENT_INTERVAL 5
 
-#define GENESIS_BLOCK_CONTENT { \
+#define GENESIS_BLOCK { \
 { /* info */ \
 0 /* index */, \
 0, /* difficulty */ \
@@ -62,7 +62,7 @@ uint8_t     hash[SHA256_DIGEST_LENGTH];
 
 } block_t;
 
-extern block_t const Genesis;
+extern block_t const _genesis;
 
 blockchain_t *blockchain_create(void);
 
