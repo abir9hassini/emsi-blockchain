4cb2b2502cf4eb23fa3422387ee20fa9ab3fbb27
abir9hassini <40744191+abir9hassini@users.noreply.github.com>
2021-05-19 22:21:35 +0100

    Update block_create.c

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -11,19 +11,20 @@ block_t *block_create(block_t const *prev,
 		      int8_t const *data, uint32_t data_len)
 {
 block_t *block;
+time_t t;
 int i;
 uint32_t max_len = data_len > BLOCKCHAIN_DATA_MAX ?
 BLOCKCHAIN_DATA_MAX : data_len;
-	
-if (prev == NULL || data == NULL)
+if (prev == NULL)
 return (NULL);
-
+if (data == NULL)
+return (NULL);
 block = (block_t *)malloc(sizeof(block_t));
-block->info.index = prev->info.index+ 1;
+block->info.index = (uint32_t)((int)(prev->info.index) + 1);
 block->info.difficulty = 0;
-block->info.timestamp = time(0);
+t = time(0);
+block->info.timestamp = (uint64_t)&t;
 block->info.nonce = 0;
-	
 for (i = 0; i < BLOCKCHAIN_DATA_MAX; i++)
 {
 block->data.buffer[i] = 0;
