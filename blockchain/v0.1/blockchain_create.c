b3223fb268f9aa3d3c5ac96f78a1118fd6fca9ec
abir9hassini <40744191+abir9hassini@users.noreply.github.com>
2021-05-19 21:55:11 +0100

    Update blockchain_create.c

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -8,11 +8,9 @@ blockchain_t *blockchain_create(void)
 {
 block_t const _genesis = GENESIS_BLOCK;
 
-blockchain_t *blockchain;
-block_t *ptr;
-ptr = (block_t *) malloc(sizeof(block_t));
+blockchain_t *blockchain = (blockchain_t *) malloc(sizeof(blockchain_t));
+block_t *ptr = (block_t *) malloc(sizeof(block_t));
 memcpy(ptr, (block_t *) &_genesis, sizeof(block_t));
-blockchain = (blockchain_t *) malloc(sizeof(blockchain_t));
 blockchain->chain = llist_create(MT_SUPPORT_TRUE);
 if (llist_add_node(blockchain->chain, ptr, ADD_NODE_FRONT) != 0)
 return (NULL);
