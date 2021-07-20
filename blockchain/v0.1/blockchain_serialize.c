ab0720ff3c97fa873bad1def8afc927c8c111948
abir9hassini <40744191+abir9hassini@users.noreply.github.com>
2021-07-20 13:37:28 +0100

    Update blockchain_serialize.c

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -7,7 +7,7 @@
  * @arg: The file in which the data will be written
  * Return: 0 upon success, or -1 upon failure
  */
-int write_block(llist_node_t ptr, unsigned int idx, void *arg)
+int write_block(llist_node_t ptr,  void *arg)
 {
 FILE *fp;
 block_t *block = ptr;
